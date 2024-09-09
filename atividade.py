31f71cb4aebcf168890698e3da457dc0bbf6ffed
kayky01silva <kaykysilva232334@gmail.com>
2024-09-09 09:54:21 -0300

    atividade04

---
+++ atividade.py
@@ -0,0 +1,7 @@
+# Crie um programa que receba três notas de um aluno e calcule a média aritmética.
+nota1 = int(input("digite a primeira nota"))
+nota2 = int(input('digite a segunda nota'))
+nota3 = int(input("digite a teceira nota"))
+
+media = (nota1 + nota2 + nota3)/3
+print(f'sua media e {media} ')
